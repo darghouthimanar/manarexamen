428678e298ccdad4d1b25ad0e2ae01c37babab4d
darghouthimanar <manardarghouthi@gmail.com>
2025-05-13 12:57:13 +0200

    Version 4 : ajout de Redis pour le caching

--- book-api/src/main/java/com/example/book_api/model/Book.java
+++ book-api/src/main/java/com/example/book_api/model/Book.java
@@ -5,16 +5,19 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
-import java.io.Serializable;
-
 @Entity
 public class Book implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    // ... tes attributs, getters, setters
+    @Id
+    private String isbn;
+    private String title;
+    private String author;
+    private double price;
 }
